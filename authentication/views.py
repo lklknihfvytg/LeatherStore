b5a141f6c62d9d0751a51a47fae8d28538ca893a
Ilya Antonenko <ilyaantonenkoilya@gmail.com>
2022-09-05 19:18:00 +0300

    5 sept commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_user(request):
+	return render(request, 'auth/login.html')
+
+
+def register(request):
+	return render(request, 'auth/register.html')
+
+
+def logout_user(request):
+	logout(request)
+	return redirect('index')
